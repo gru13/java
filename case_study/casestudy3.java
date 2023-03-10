21cf185d0e56593d69711169483760841d74e0b4
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-10 05:17:26 +0000

    casestudy3_skeleten done
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

--- case_study/casestudy3.java
+++ case_study/casestudy3.java
@@ -3,8 +3,23 @@ public class casestudy3{
 	public static void main(String args[]){
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
-		int arr[][] = new int[n][n];
-
+		int arr[][] = new int[n][2*n-1];
+		for(int i = 0; i<n;i++){
+			for(int j =0;j<2*n-1;j++){
+				if(i==0){
+					arr[i][j]=1;
+					continue;
+				}
+				else if(i==1){
+					arr[i][0]=1;
+					arr[i][1]=2;
+					arr[i][2]=1;
+				}
+				else{
+					
+				}
+			}
+		}
 
 	}
 }
