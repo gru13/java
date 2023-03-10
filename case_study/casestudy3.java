21245977929db75fb739451f59b431be3c3c73f0
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-10 06:24:17 +0000

    .
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

--- case_study/casestudy3.java
+++ case_study/casestudy3.java
@@ -4,7 +4,7 @@ public class casestudy3{
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
 		int arr[][] = new int[n][n];
-		
+
 		for(int i = 0; i<n;i++){
 			for(int j = 0;j<i;j++){
 				if(i==0){
@@ -15,7 +15,7 @@ public class casestudy3{
 						arr[i][j] = 1;
 					}
 					else{						
-						arr[i][j] = arr[(i-1)][j] + arr[(i-1)][(j-1)];
+						arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
 					}
 				}
 			}
