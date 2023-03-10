21bee404c163d44b69bb95d7c00d6d7410ae4288
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-10 06:29:36 +0000

    casestudy4 done
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

---
+++ case_study/casestudy4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class casestudy4{
+    public static void main(String args[]){
+	    Scanner input = new Scanner(System.in);
+	    String str1 = input.nextLine();
+	    String str2 = input.nextLine();
+	    String passwd = str1.substring(0,str1.indexOf(" "))+str2+str1.substring(str1.indexOf(" ")+1,str1.length());
+	    System.out.println(passwd); 
+    }
+}
