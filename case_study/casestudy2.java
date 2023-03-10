da7e5304990e9cc233d88ca3d361d80792625e36
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-10 05:10:12 +0000

    casestudy2
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

--- case_study/casestudy2.java
+++ case_study/casestudy2.java
@@ -1,1 +1,17 @@
-public class casestudy2{}
+import java.util.Scanner;
+
+public class casestudy2{
+	public static void main(String args[]){
+		// milk man
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		int L5 = num/5;
+		System.out.println("5L--"+L5);
+		num %= 5;
+		int L3 = num/3;
+		System.out.println("3L--"+L3);
+		num %= 3;
+		System.out.println("1L--"+num);
+		input.close();
+	}
+}
---
+++ case_study/casestudy3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class casestudy3{
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int arr[][] = new int[n][n];
+
+
+	}
+}
