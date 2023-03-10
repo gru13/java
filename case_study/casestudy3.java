5ff06e708543b9878fcde368d1766976b2e1a8fd
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-10 06:20:25 +0000

    casestudy3 done
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

--- case_study/casestudy3.java
+++ case_study/casestudy3.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 public class casestudy3{
 	public static void main(String args[]){
 		Scanner input = new Scanner(System.in);
-		// int n = input.nextInt();
-		int n = 5;
+		int n = input.nextInt();
 		int arr[][] = new int[n][n];
+		
 		for(int i = 0; i<n;i++){
 			for(int j = 0;j<i;j++){
-				if(i<2){
+				if(i==0){
 					arr[i][j] = 1;
 				}
 				else{
-					if(j==0&j==i-1){
+					if(j==0 || j==i-1){
 						arr[i][j] = 1;
 					}
-					else{
-						arr[i][j] = arr[i-1][j]+arr[i][j-1];
+					else{						
+						arr[i][j] = arr[(i-1)][j] + arr[(i-1)][(j-1)];
 					}
 				}
 			}
