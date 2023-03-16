94d388975bfe4c38614f6dc0c01a435b51194da5
gru13 <guruprasath1302@gmail.com>
2023-03-16 18:48:10 +0530

    pascalTriangle

---
+++ _15code/pascalTriangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class pascalTriangle{
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int arr[][] = new int[n][n];
+
+		for(int i = 0; i<n;i++){
+			for(int j = 0;j<i;j++){
+				if(i==0){
+					arr[i][j] = 1;
+				}
+				else{
+					if(j==0 || j==i-1){
+						arr[i][j] = 1;
+					}
+					else{						
+						arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
+					}
+				}
+			}
+		}
+		// printing
+		for(int i = 0; i<n;i++){
+			for(int j = 0;j<i;j++){
+				System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	input.close();
+	}
+
+}
