6c8881d7e962d1329131a3856667f11f54151395
Guruprasath <guruprasath1302@gmail.com>
2023-03-17 06:19:15 +0000

    candies added

---
+++ _15code/Candies.java
@@ -0,0 +1,19 @@
+//  leet code question
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/
+class Candies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> rest =  new ArrayList<Boolean>(); 
+        for(int i =0 ; i<candies.length;i++){
+            rest.add(max(candies,candies[i]+extraCandies));
+        }
+        return rest;
+    }
+    public static boolean max(int[] arr, int val){
+        for(int value:arr){
+            if(value>val){
+                return false;
+            }
+        }
+        return true ;
+    }
+}
