c369ea2e10a8948d9db9514b2f1463ea7adbadb7
gru13 <guruprasath1302@gmail.com>
2023-03-17 22:48:54 +0530

    extra.Matrix
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- extra/Matrix.java
+++ extra/Matrix.java
@@ -19,11 +19,48 @@ public class Matrix{
     }
 
     void display(){
+        System.out.println();
         for(int row[]:Mat){
             for(int val: row){
                 System.out.print(val+"\t");
             }
             System.out.println();
+        }
+        System.out.println();
+    }
+
+    Matrix add(Matrix a){
+        for(int i = 0 ;i<a.Mat.length;i++){
+            for(int j = 0;j<a.Mat[0].length;j++){
+                a.Mat[i][j] += Mat[i][j];
+            }
+        }
+        return a;
+    }
+    
+    Matrix subtract(Matrix a){
+        for(int i = 0 ;i<a.Mat.length;i++){
+            for(int j = 0;j<a.Mat[0].length;j++){
+                a.Mat[i][j] -= Mat[i][j];
+            }
+        }
+        return a;
+    }
+    
+    Matrix s_mul(Matrix a){
+        for(int i = 0 ;i<a.Mat.length;i++){
+            for(int j = 0;j<a.Mat[0].length;j++){
+                a.Mat[i][j] *= Mat[i][j];
+            }
+        }
+        return a;
+    }
+
+    void s_mul(int val){
+        for(int i = 0 ;i<Mat.length;i++){
+            for(int j = 0;j<Mat[0].length;j++){
+                Mat[i][j] *= val;
+            }
         }
     }
 
@@ -33,9 +70,10 @@ public class Matrix{
         m1.Create();
         m1.display();
         m2.Create();
-        System.out.println();
-        m1.display();
-        System.out.println();
         m2.display();
+        Matrix M3 = m1.s_mul(m2);
+        m1.s_mul(7);
+        M3.display();
+        m1.display();
     }
 }
